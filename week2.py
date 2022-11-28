bba00f4b1237de25f62511b43b42cdedd965fd6e
Brett Cantrell <cantrell.brett8892@gmail.com>
2022-11-28 17:54:14 -0600

    Updated file

--- week2.py
+++ week2.py
@@ -19,8 +19,11 @@
 import random
 
 greeting_message = "Thank you for choosing us to help plan your upcoming trip."
+
 print(greeting_message)
+
 ending_message = "We hope you enjoy your trip!"
+
 trip_destinations = ["Oklahoma City", "New York", "Miami"]
 restaurant_type_for_trip = ["Italian", "Mexican", "Indian"]
 transportation_for_trip = ["Bus", "Uber","Car Rental"]
@@ -40,12 +43,12 @@ confirm_choice = input("Please enter yes/no to confirm these travel options. ")
 def new_dest(confirm_choice):  
     while confirm_choice != "yes":
         destination = random.choice(trip_destinations)
-        confirm_choice = input(f"{destination} is a trip destination. Does this work for you? ")
+        confirm_choice 
         if input == "no":
             destination.remove(trip_destinations)
         else:
             print(f"You have selected {destination} as the destination for your trip. ")
-    return destination
+    return trip_destinations
 
 new_dest(confirm_choice)
 
@@ -57,7 +60,7 @@ def new_restaurant(confirm_choice):
             restaurant.remove(restaurant_type_for_trip)
         else:
             print(f"You have selected {restaurant} as the best place to eat during your trip. ")
-    return restaurant
+    return restaurant_type_for_trip
 new_restaurant(confirm_choice)
 
 def new_trans(confirm_choice):  
@@ -68,7 +71,7 @@ def new_trans(confirm_choice):
             transportation.remove(transportation_for_trip)
         else:
             print(f"You have selected {transportation} as a way to get around. ")
-    return transportation
+    return transportation_for_trip
 new_trans(confirm_choice)
 
 def new_entertainment(confirm_choice):  
@@ -79,7 +82,7 @@ def new_entertainment(confirm_choice):
             entertainment.remove(entertainment_for_trip)
         else:
             print(f"You have selected {entertainment} as a fun activity for you to do while on your trip. ")
-    return entertainment
+    return entertainment_for_trip
 new_entertainment(confirm_choice)
 
 print(ending_message)
